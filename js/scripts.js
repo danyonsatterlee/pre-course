0908cdf031efca4bc9a5349b31a4b70f39010108
danyonsatterlee <danyon.satterlee@gmail.com>
2017-03-31 11:56:32 -0700

    added for loop
    runs through a for loop of sayings at the bottom

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,36 @@ $(function() {
   showFortuneBtn.addEventListener("click", createAndDisplayFortune)
 
 
+
   function createAndDisplayFortune(){
+    if(document
+      .getElementById("ask").value == null || document.getElementById("ask").value == "")
+    {
+      alert("I can't reviel your future if you don't ask a question!");
+      return false;
+    }
 
-    let fortune = [
-      `It is not wise`, `This is not your path`, `Good things are to come`, `The future is cloudy`, `Are you crazy?`, `You are headed in the right direction`, `It is your destiny`, `Turn back now`
-    ];
 
-    let resultDiv = document.getElementById('result')
+    else {
+      let fortune = [
+        `It is not wise`, `This is not your path`, `Good things are to come`, `The future is cloudy`, `Are you crazy?`, `You are headed in the right direction`, `It is your destiny`, `Turn back now`
+      ];
 
-    resultDiv.innerHTML = `<h2>Behold Your Fortune</h2><h3>${fortune[Math.floor(Math.random()* fortune.length)]}</h3>`;
+      let resultDiv = document
+      .getElementById('result')
 
+      resultDiv.innerHTML = `<h2>Behold Your Fortune</h2><h3>${fortune[Math.floor(Math.random()* fortune.length)]}</h3>`;
+
+    }
   }
 });
+
+let advertise = ['Behold the Wonder!', 'Uncanny!', 'Beyond Belief!', 'The Truth Will Bewilder!']
+let i = 0;  // the index of the current item to show
+
+setInterval(function() {            // setInterval makes it run repeatedly
+    document
+        .getElementById('advertise')
+        .innerHTML = advertise[i++];    // get the item and increment
+    if (i == advertise.length) i = 0;   // reset to first element if you've reached the end
+}, 1000);
